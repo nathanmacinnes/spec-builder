c9a83eaea75d52f6c590313b31f982e5461ce539
Nathan MacInnes <nathan@macinn.es>
2014-11-19 14:01:06 +0000

    initial

---
+++ README.md
@@ -0,0 +1,88 @@
+# spec-builder #
+
+_ Roll your own test/spec assertions syntax _
+
+**Step 1:** Pick an awesome verb
+````js
+var prescribe = require("spec-builder")();
+````
+
+**Step 2:** Give it some initial assertions
+````js
+var prescribe = require("spec-builder")({
+    "should.equal" : assert.equal,
+    "should.not.equal" : assert.notEqual
+});
+````
+
+**Step 3:** Add a `that` property if needed
+````js
+prescribe.that = prescribe;
+````
+
+**Step 4:** Start using it
+````js
+prescribe.that(something).should.equal(somethingElse);
+````
+
+**Step 5:** Add an `and` property
+````js
+prescribe.add("and");
+presecribe.that(1).should.equal(1).and.should.not.equal(2);
+````
+
+**Step 6:** Add more complex assertions
+````js
+presecribe.add("has.a.property", function (actual, property, value, message) {
+    if (arguments.length === 3) {
+        message = value;
+    }
+    assert.ok(property in actual, message);
+    if (arguments.length === 4) {
+        assert.equal(actual[property], value, message);
+    }
+});
+prescribe.that([1]).has.a.property("length")
+    .and.has.a.property(0, 1);
+````
+
+**Step 7:** Export and publish
+````js
+module.exports = prescribe;
+````
+
+## ¿Habla español? ##
+
+````js
+var esperar = {};
+esperar.que = require("spec-builder")({
+    "es.egual.a" : assert.equal,
+    "no.es.egual.a" : assert.notEqual
+});
+esperar.que.add("y");
+esperar.que(1).es.egual.a(1).y.no.es.egual.a(2);
+````
+
+## License ##
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Nathan MacInnes
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ test/test-spec-builder.js
@@ -0,0 +1,181 @@
+var assert = require("assert"),
+    Chance = require("chance").Chance,
+    injectr = require("injectr"),
+    pretendr = require("pretendr");
+
+describe("spec-builder", function () {
+    var cb,
+        mockInspect,
+        random,
+        assertionInstance;
+    beforeEach(function () {
+        cb = pretendr();
+        mockInspect = pretendr();
+        assertionInstance = injectr("../lib/spec-builder.js", {
+            util : {
+                inspect : mockInspect.mock
+            }
+        });
+        random = new Chance(this.currentTest.title.charAt(0));
+    });
+
+    function randomString() {
+        // make sure . character isn't included
+        return random.string({
+            pool : "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                "0123456789"
+        });
+    }
+
+    it("returns a function", function () {
+        assert.equal(typeof assertionInstance(), "function");
+    });
+    describe("instance", function () {
+        var test;
+        beforeEach(function () {
+            test = assertionInstance();
+        });
+        describe("add method", function () {
+            it("allows adding assertion methods", function () {
+                var method = randomString();
+                test.add(method, cb.mock);
+                cb.calls.length = 0;
+                test(1)[method]();
+                assert.equal(cb.calls.length, 1);
+            });
+            it("doesn't call the method too early", function () {
+                var method = randomString();
+                test.add(method, cb.mock);
+                assert.equal(cb.calls, 0);
+            });
+            it("allows adding chains", function () {
+                var route = random.n(randomString, 3, {
+                    pool : "abcdefghijklmnopqrstuvwxyz"
+                });
+                test.add(route.join("."), cb.mock);
+                cb.calls.length = 0;
+                test(1)[route[0]][route[1]][route[2]](1);
+                assert.equal(cb.calls.length, 1);
+            });
+            it("passes the 'actual' value to created methods", function () {
+                var method = randomString(),
+                    value = {};
+                test.add(method, cb.mock);
+                test(value)[method]();
+                assert.equal(cb.calls[0].args[0], value);
+            });
+            it("passes the 'expected' values to created methods", function () {
+                var method = randomString(),
+                    expectedArgs = random.n(randomString, random.natural({
+                        min : 1,
+                        max : 10
+                    }));
+                test.add(method, cb.mock);
+                test({})[method].apply(test[method], expectedArgs);
+                assert.deepEqual(
+                    cb.calls[0].args.slice(1, expectedArgs.length + 1),
+                    expectedArgs);
+            });
+            it("makes added methods return the original", function () {
+                var method = randomString(),
+                    t;
+                test.add(method, cb.mock);
+                t = test(1);
+                assert.equal(t[method](), t);
+            });
+            it("runs added methods in context of the original", function () {
+                var method = randomString(),
+                    t;
+                test.add(method, cb.mock);
+                t = test(1);
+                t[method]();
+                assert.equal(cb.calls[0].context, t);
+            });
+            it("adds a concatenator with only a single argument", function () {
+                var prop = randomString(),
+                    t;
+                test.add("equals", function () {});
+                test.add(prop);
+                t = test(1);
+                assert.equal(t.equals()[prop], t);
+            });
+            describe("to display an assertion message", function () {
+                var args,
+                    method,
+                    route,
+                    val;
+                beforeEach(function () {
+                    args = random.n(randomString, random.natural({
+                        min : 1,
+                        max : 10
+                    }));
+                    val = randomString();
+                    route = random.n(randomString, 3);
+                    test.message = cb.mock;
+                    cb.fake(randomString);
+                    route = random.n(randomString, 3);
+                    method = pretendr();
+                    test.add(route.join("."), method.mock);
+                    test(val)[route[0]][route[1]][route[2]].apply(null, args);
+                });
+                it("passes the route to .message()", function () {
+                    assert.deepEqual(cb.calls[0].args[0], route);
+                });
+                it("passes the original value to .message()", function () {
+                    assert.equal(cb.calls[0].args[1], val);
+                });
+                it("passes the arguments to .message()", function () {
+                    assert.deepEqual(cb.calls[0].args.slice(2), args);
+                });
+                it("passes the result to the method", function () {
+                    var methodArgs = method.calls[0].args;
+                    assert.equal(methodArgs[methodArgs.length - 1],
+                        cb.calls[0].returned);
+                });
+            });
+        });
+        describe("message method", function () {
+            it("calls util.inspect for all but the 1st argument", function () {
+                var args = random.n(randomString, random.natural({
+                    min : 1,
+                    max : 10
+                }));
+                test.message.apply(null, [[]].concat(args));
+                args.forEach(function (arg, index) {
+                    assert.equal(mockInspect.calls[index].args[0], arg);
+                });
+            });
+            it("returns the first inspect result at the start", function () {
+                var msg;
+                mockInspect.fake(randomString);
+                msg = test.message([], randomString);
+                assert.equal(msg.indexOf(mockInspect.calls[0].returned), 0);
+            });
+            it("returns comma-separated arg results at the end", function () {
+                var args = random.n(randomString, random.natural({
+                        min : 1,
+                        max : 10
+                    })),
+                    joined,
+                    msg;
+                mockInspect.fake(randomString);
+                msg = test.message.apply(null, [[]].concat(args));
+                joined = mockInspect.calls.slice(1).map(function (call) {
+                    return call.returned;
+                }).join(", ");
+                assert.equal(msg.indexOf(joined),
+                    msg.length - joined.length);
+            });
+            it("returns a space-joined route in the middle", function () {
+                var msg,
+                    route = random.n(randomString, random.natural({
+                        min : 1,
+                        max : 10
+                    }));
+                mockInspect.returnValue("!");
+                msg = test.message(route, {}, {});
+                assert.equal(msg, "! " + route.join(" ") + " !");
+            });
+        });
+    });
+});
